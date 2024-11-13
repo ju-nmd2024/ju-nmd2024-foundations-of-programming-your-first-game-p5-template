e73962311153ed36dfae8a40aa75610708361451
bardiasalekani <saba24pi@student.ju.se>
2024-11-13 17:07:44 +0100

    Create character_move.js
    this my character movment code

---
+++ character_move.js
@@ -0,0 +1,74 @@
+
+background(255, 255, 255);
+
+let x = 100;
+let y = 100;
+let speed = 4;
+let normalSpeed = 4; 
+let slowSpeed = 1;
+
+function cake (x ,y) {
+    
+ // cake 
+ strokeWeight(2);
+ fill(101, 67, 33);
+ rect(x - 70 , y - 15, 200 , 130 , 7 );
+
+ // brighter part of the cake
+ push(); 
+ fill(139, 89, 39);
+ noStroke();
+ // up part 
+ rect(x - 70, y + 15, 200 , 25 );
+ // middle part 
+ rect(x - 70, y + 50, 200 , 25);
+ // last part 
+ rect(x - 70, y + 85, 200 , 25);
+ pop();
+
+ // with cream 
+ fill(255, 245, 245);
+ noStroke();
+ ellipse(x + 30  , y - 10 , 45 , 18);
+ stroke(0, 0, 0);
+ strokeWeight(1);
+ ellipse(x + 30 , y - 10 , 30, 15);
+
+ // cherry shadow
+ fill(180, 0, 0);
+ noStroke();
+ ellipse(x + 30 , y - 15 , 22, 22);
+ // cherry 
+ fill(220, 20, 60);
+ ellipse(x + 28, y - 16, 20, 20);
+ // cherry shine 
+ fill(255, 255, 255);
+ ellipse(x + 25 , y - 20 , 6, 6);
+ strokeWeight(3);
+ fill(255, 0, 0);
+ line(x + 120 , y + 60, x + 130, y + 80);
+
+
+}
+
+function setup () {
+ createCanvas(800,600);
+ background(255, 255, 255);
+ 
+  
+}
+function draw (){
+
+ background(255, 255, 255);
+ if (keyIsDown(32) || keyIsDown(UP_ARROW)) {
+    speed = slowSpeed;
+ } else {
+    speed = normalSpeed;
+ }
+ cake (x, y);
+ if (y < 550){
+    y = y + speed;
+ }
+}     
+
+// use the up arrow key or space to control the speed of the cake
