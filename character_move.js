8837160d097709b5c9abe2efe4de30fcf5f5a778
bardiasalekani <saba24pi@student.ju.se>
2024-11-19 07:55:42 +0100

    Update character_move.js

--- character_move.js
+++ character_move.js
@@ -6,19 +6,37 @@ let normalSpeed = 4;
 let slowSpeed = 1;
 let gameState = 'start';
 let difficulty = '';
+let lastColorChange = 0;
+let colorChange = 1000;
+
 
 // plate 
 let plates = [
    {x: 200, y: 550, isGreen:false},
    {x: 400, y: 550, isGreen:false},
    {x: 600, y: 550, isGreen:false},
-]
+];
 
 function setup () {
  createCanvas(800,600);
 }
 
-function setPlateColor(){
+function gameCondition() {
+   if(y >= height - 150) {
+      for (let plate of plates) {
+         if (x >= plate.x - 70 && x <= plate.x + 100) { 
+            if ( y >= plate.y - 20) {
+               if (plate.isGreen) {
+                  gameState = 'win';
+               } else {
+                  gameState = 'gameover';
+               }
+            }
+         }
+      }
+   }
+}
+function setPlateColor() {
    // red plate 
    plate[0].isGreen = false;
    plate[1].isGreen = false;
@@ -41,6 +59,27 @@ function setPlateColor(){
 
 } 
 
+function updateHard (){
+   if ( difficulty === 'hard' && millis() - lastColorChange >= colorChange){
+      for (let plate of plates) {
+         plate.isGreen = false;
+      }
+      let greenPlateIndex = floor(random(3));
+      plates[greenPlateIndex].isGreen = true;
+      lastColorChange = millis();
+   }
+}
+function gameOver(){
+   if (y >= height - 150){
+      for (let plate of plates){
+         if (x >= plate.x - 70 && x <= plate.x + 100){
+            if (!plate.isGreen && y >= plate.y - 20){
+               gameState = 'gameover';
+            }
+         }
+      }
+   }
+}
 function cake (x ,y) {
     
    // cake 
@@ -85,18 +124,124 @@ function cake (x ,y) {
   
   }
   
-function draw (){
+function draw () {
 
  background(255, 255, 255);
- if (keyIsDown(32) || keyIsDown(UP_ARROW)) {
-    speed = slowSpeed;
- } else {
-    speed = normalSpeed;
- }
- cake (x, y);
- if (y < 550){
-    y = y + speed;
- }
-}     
 
-// use the up arrow key or space to control the speed of the cake
+// start screen 
+if (gameState === 'start'){
+   textAlign(CENTER);
+   textSize(32);
+   fill(0, 0, 0);
+   text("cake drop game", width/2, 200);
+   
+   fill(255, 200, 255);
+   rect(350, 250, 100, 40, 5);
+   fill(0, 0, 0);
+   textSize(20);
+   textAlign(CENTER, CENTER);
+   text("start", 400, 270);
+
+   // difficulty screen 
+} else if (gameState === 'difficulty') {
+   textAlign(CENTER);
+   textSize(32);
+   fill(0, 0, 0);
+   text("Select Difficulty", width/2, 200);
+
+   fill(255, 200, 255);
+   rect(250, 250, 100, 40, 5);
+   rect(350, 250, 100, 40, 5);
+   rect(450, 250, 100, 40, 5);
+
+   fill(0, 0, 0);
+   textSize(20);
+   textAlign(CENTER, CENTER);
+   text("EASY", 300, 270);
+   text("MEDIUM", 400, 270);
+   text("HARD", 500, 270);
+   
+   // gane play screen 
+} else if (gameState === 'playing') {
+   updateHard();
+
+   for (let plate of plates) {
+      if (plate.isGreen) {
+         fill(0, 255, 0);
+      } else {
+         fill(255, 0 ,0);
+      }
+      rect(plate.x, plate.y, 100, 20);
+   }
+}
+// movment 
+if (keyIsDown(UP_ARROW)) {
+   y = y - 5; 
+} else {
+   y = y + speed;
+}
+if (keyIsDown(LEFT_ARROW)) {
+   x = x - 5;
+}
+if (keyIsDown(RIGHT_ARROW)) {
+   x = x + 5; 
+}
+if (x < 70) x = 70;
+if (x > width - 70) x = width - 70;
+if (y < 0) y = 0;
+if (y > height - 50) y = height - 50;
+
+gameOver();
+
+cake(x, y);
+
+  if ( gameState === 'gameover') {
+   textAlign(CENTER);
+   textSize(32);
+   fill(255, 0, 0);
+   text("Game Over!", width/2, height/2);
+
+   //restart 
+   fill(255, 200, 255);
+   rect(350, 350, 100, 40, 5);
+   fill(0, 0, 0);
+   textSize(20);
+   textAlign(CENTER,CENTER);
+   text("RESTART", 400, 370);
+ }
+}
+
+// mouse 
+
+function mousePressed() {
+   if (gameState === 'start') {
+      if (mouseX > 350 && mouseX < 450 && mouseY > 250 && mouseY < 290) {
+         gameState = 'difficulty';
+      }
+   } else if (gameState === 'difficulty') {
+      if (mouseY > 250 && mouseX < 290) {
+         if (mouseX > 250 && mouseX < 350) {
+            difficulty = 'easy';
+            gameState = 'playing';
+            setPlateColors();
+         } else if ( mouseX > 350 && mouseX < 450) {
+            difficulty = 'medium';
+            gameState = ' playing';
+            setPlateColors();
+         } else if ( mouseX > 450 && mouseX < 550) {
+            difficulty = 'hard';
+            gameState = 'playing';
+            setPlateColors();
+            lastColorChange = millis();
+         }
+         
+      } 
+   } else if (gameState === 'gameover') {
+      if ( mouseX > 350 && mouseX <450 && mouseY > 350 && mouseY < 390) {
+         x = 100;
+         y = 100;
+         gameState = start;
+      }
+   }
+} 
+  
